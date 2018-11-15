6ca03973d47e3ae2c686f9d725037cc0596ff358
Kembly Quirós Araya <kquirosaraya@gmail.com>
2018-11-14 23:35:44 -0600

    Creating Retrofit

---
+++ MobileApp/app/src/main/java/HttpUserServiceAPI/HttpUserService.java
@@ -0,0 +1,8 @@
+package HttpUserServiceAPI;
+
+/**
+ * Created by kembl on 14/11/2018.
+ */
+
+public interface HttpUserService {
+}
--- MobileApp/app/src/main/java/com/example/kembl/chatcito/MainActivity.java
+++ MobileApp/app/src/main/java/com/example/kembl/chatcito/MainActivity.java
@@ -2,12 +2,28 @@ package com.example.kembl.chatcito;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText username, password;
+    Button loginAction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        username = findViewById(R.id.usernameTxt);
+        password = findViewById(R.id.passwordTxt);
+        loginAction = findViewById(R.id.loginBtn);
+
+        loginAction.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
