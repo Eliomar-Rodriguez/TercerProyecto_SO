50b10ff05be150961ab31ae1f59d1fab0a1f4953
Kembly Quirós Araya <kquirosaraya@gmail.com>
2018-11-17 00:30:28 -0600

    tareas por lotes

--- MobileApp/app/src/main/java/com/example/kembl/chatcito/CurrentChatActivity.java
+++ MobileApp/app/src/main/java/com/example/kembl/chatcito/CurrentChatActivity.java
@@ -1,11 +1,15 @@
 package com.example.kembl.chatcito;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import ChatMessage.Message;
 import ChatMessage.MessageAdapter;
@@ -14,33 +18,32 @@ public class CurrentChatActivity extends AppCompatActivity {
     EditText inputMessageText;
     MessageAdapter messageAdapter = new MessageAdapter(this);
     Intent intent;
-    String usernameKey;
+    String receiverUsernameKey;
     ListView messagesView;
+    int works = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_current_chat);
 
+        intent = getIntent();
+        receiverUsernameKey = intent.getStringExtra(getString(R.string.receiverKey));
+
         inputMessageText = findViewById(R.id.inputEditText);
         messagesView = findViewById(R.id.messages_view);
-
-        intent = getIntent();
-        usernameKey = intent.getStringExtra(getString(R.string.userKey));
 
         messageAdapter = new MessageAdapter(this);
         messagesView = findViewById(R.id.messages_view);
         messagesView.setAdapter(messageAdapter);
     }
 
-    public void sendMessage(View view){
+    public void sendMessage(){
         String message = inputMessageText.getText().toString();
         if (message.length() > 0) {
             //enviar mensaje
             final Message mesage = new Message(message, true);
             messageAdapter.add(mesage);
-            inputMessageText.getText().clear();
-            receiveMessage();
         }
     }
 
@@ -64,5 +67,36 @@ public class CurrentChatActivity extends AppCompatActivity {
         } catch (Exception e){
             e.printStackTrace();
         }
+    }
+
+    public void WorksAlertDialog(View view){
+        final EditText input = new EditText(this);
+        input.setInputType(InputType.TYPE_CLASS_NUMBER);
+        new AlertDialog.Builder(CurrentChatActivity.this)
+            .setTitle("Antes de enviar")
+            .setMessage("Cuantos trabajos por lotes desea enviar (entre 1 hasta 999)")
+            .setView(input)
+            .setCancelable(true)
+            .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    String num = input.getText().toString();
+                    if(!num.equals(""))
+                        works = Integer.parseInt(num);
+                    sendMassiveMessages(works);
+                }
+            }).show();
+    }
+
+    public void sendMassiveMessages(int tot){
+        if(tot > 1 && tot < 1000){
+            for (int i = 0; i < tot; i++) {
+                sendMessage();
+            }
+        } else {
+            Toast.makeText(getApplicationContext(), "Mensaje enviado una vez", Toast.LENGTH_SHORT).show();
+            sendMessage();
+        }
+        inputMessageText.getText().clear();
     }
 }
